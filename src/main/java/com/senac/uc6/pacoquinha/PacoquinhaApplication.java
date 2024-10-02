d8d85f7e54624ffa79ae660ce8772432d2e5e6e4
FRANCISCO CIRIACO EVANGELISTA NETO <franscisco.ceneto@senacsp.edu.br>
2024-10-02 20:39:01 -0300

    commet inicial

---
+++ src/main/java/com/senac/uc6/pacoquinha/PacoquinhaApplication.java
@@ -0,0 +1,14 @@
+package com.senac.uc6.pacoquinha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PacoquinhaApplication {
+
+	public static void main(String[] args) {
+	System.out.println("Ola, mundo");
+		SpringApplication.run(PacoquinhaApplication.class, args);
+	}
+
+}
